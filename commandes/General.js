2a49d73307bd332a71e459848d506cf137a095dd
Deltahmd <magydalinemutinda@gmail.com>
2024-06-07 22:22:50 +0300

    Update General.js

--- commandes/General.js
+++ commandes/General.js
@@ -55,12 +55,12 @@ zokou({ nomCom: "dev", categorie: "General", reaction: "💞" }, async (dest, zk
     const { ms, mybotpic } = commandeOptions;
 
     const devs = [
-      { nom: "Djalega++", numero: "22559763447" },
-      { nom: "᚛M๏𝓷keℽ D Lบffy᚜", numero: "22891733300" },
-      // Ajoute d'autres développeurs ici avec leur nom et numéro
+      { nom: "Deltah Alpha", numero: "254796266758" },
+      { nom: "Beltah Tech", numero: "254114141192" },
+       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
-    let message = "👋 welcome to Zokou ! here is the dev :\n\n";
+    let message = "👋 welcome to Deltah-Md ! here is the dev :\n\n";
     for (const dev of devs) {
       message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
     }
@@ -95,7 +95,7 @@ zokou({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOpti
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
   repondre("look on pm sir ")
-  await zk.sendMessage(auteurMessage,{text : `https://chat.whatsapp.com/H6oeuhfSMtV1Orjmf2NVnl`},{quoted :ms})
+  await zk.sendMessage(auteurMessage,{text : `https://wa.me/254796266758`},{quoted :ms})
 
 })
 
