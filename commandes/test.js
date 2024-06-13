2db6a492f78681c429f934396384514aebff18d9
Deltahmd <magydalinemutinda@gmail.com>
2024-06-13 15:45:04 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello my name is  *Zokou* \n\n ' + "i'm a whatsapp bot multi-device";
-    let d = ' by *Djalega++*';
+    let z = 'Hello my name is  *Deltah* \n\n ' + "i'm a whatsapp bot multi-device";
+    let d = ' by *Deltah-Tech*';
     let varmess = z + d;
     var img = 'https://wallpapercave.com/uwp/uwp3842939.jpeg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
@@ -19,8 +19,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *Djalega++*'
+   let z ='Salut je m\'appelle *Deltah* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Deltah-Tech*'
       let varmess=z+d
       var img='https://wallpapercave.com/uwp/uwp3842939.jpeg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
