c7c1560ae61c9003659d6d7d0a484a2d58fd669d
Supreme-Tech <magydalinemutinda@gmail.com>
2024-06-16 00:48:09 +0300

    Update rank.js

--- commandes/rank.js
+++ commandes/rank.js
@@ -134,7 +134,7 @@ zokou( {
     
     
          let msg = `
-┏━━━┛ DH-Rang ┗━━━┓
+┏━━━┛ SM-Rang ┗━━━┓
          
     *Name :* @${auteurMsgRepondu.split("@")[0]}
     
@@ -210,7 +210,7 @@ zokou( {
     
     
          let msg = `
-┏━━━┛ DH-Rang ┗━━━┓
+┏━━━┛ SM-Rang ┗━━━┓
      
   *Name :* ${nomAuteurMessage}
 
@@ -251,7 +251,7 @@ zokou( {
     const {ms , mybotpic} = commandeOptions ;
 
 
-       let msg = `┏━━┛ DH-top-rang ┗━━┓\n\n`
+       let msg = `┏━━┛ SM-top-rang ┗━━┓\n\n`
        
       let topRanks = await getBottom10Users() ;
         let mention = [] ;
