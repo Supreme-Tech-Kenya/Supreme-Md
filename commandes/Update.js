4cdc123b4af78f9b104059631687c948d35202cf
Safaribenjamin <167427355+Safaribenjamin@users.noreply.github.com>
2024-06-19 13:31:53 +0300

    Update.js

---
+++ commandes/Update.js
@@ -0,0 +1,104 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "wagroup", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Hello 🤍 \n\nClick The link below to Join *SUPREME-MD* WhatsApp Testing Group\n\n';
+    let d = 'https://chat.whatsapp.com/Bw2m61sYFUyB5Zp1h8DznX';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Hello 🤍 \n\nClick The link below to Join *SUPREME-MD* WhatsApp Channel\n\n';
+    let d = 'https://whatsapp.com/channel/0029Vah7UNT6BIElZKwvzY43';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "developer", reaction: "🌟", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *HERE ARE MY OWNER INFO* \n\n✇ *NAME* : BENJAMIN SAFARI \n✇ *AGE* : 21 years\n✇ *SEX* : MALE\n✇ *RELIGION* : Christian\n✇ *NATIONALITY* :Kenyan 🇰🇪\n✇ *FAV TEAM* : LIVERPOOL FC\n✇ *REAL LIFE* :AM JUST EMPLOYED ,NEVER LEARNED CODDING AT ALL.\n✇ *HOBBY* : ELECTRICAL INSTALLATION  *CONTACT LIL BENJAMIN:👇 👇 \n'; https://wa.me/254769702239\n\n **THANK YOU FOR CHOOSING SUPREME-MD.* ';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "test", reaction: "✅", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *SUPREME-MD BOT IS CURRENTLY ACTIVE ☹︎*\n';
+    let d = ' ❄︎ *_MADE BY SUPREME-TECH* ❄︎';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a73725681808deb94b76f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "bot", reaction: "🤖", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *SUPREME MD MAIN FEATURES* \n\n 1.Auto view status✅\n 2.Always online✅\n 3.A1 chat gpt enabled \n 4.Download songs/images✅\n 5.Open view once photos✅\n 6.Antidelete messages✅\n 7.Auto block contacts✅\n 8.Auto bluetick✅\n\n';
+    let d = ' *THANK YOU FOR LOVING SUPREME-MD*';
+    let varmess = z + d;
+    var img ='https://telegra.ph/file/a73725681808deb94b76f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "g*y", reaction: "🏳️‍🌈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *F*CK MEHN,,, THIS GUY IS G*Y🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈*\n';
+    let d = 'WHY WOULD YOU REQUEST THAT COMMAND IF NOT';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a73725681808deb94b76f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "check", reaction: "🗿", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *SUPREME-MD UPDATES*\n\n';
+    let d = 'Checking for updates...⏳\n\nUpdating new version...⏳\n\ndone....✅\n\n *SUPREME-MD NEW VERSION SUCCESSFULLY INSTALLED...* ';
+    let varmess = z + d;
+    var img = ''https://telegra.ph/file/a73725681808deb94b76f.jpg;
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "version", reaction: "⏳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *YOU ARE CURRENTLY USING SUPREME-MD\n\n';
+    let d = '✅latest version installed...';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a73725681808deb94b76f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Djalega++*'
+      let varmess=z+d
+      var img='https://wallpapercave.com/uwp/uwp3842939.jpeg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */
